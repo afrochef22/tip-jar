b946a1b176ed8f7d1c724ff542c2f222bbb3b6c4
afrochef22 <70339140+afrochef22@users.noreply.github.com>
2024-01-28 17:03:22 -0800

    updateEmployee endpoint created

---
+++ pages/api/updateEmployee/[id].js
@@ -0,0 +1,51 @@
+import clientPromise from "../../../lib/mongodb";
+import { ObjectId } from "mongodb";
+
+export default async (req, res) => {
+	const id = req.query.id;
+	console.log(req.query);
+	const data = req.body; // Assuming data is sent in the request body
+
+	if (!ObjectId.isValid(id)) {
+		return res.status(400).json({ error: "Invalid Employee ID: ", id });
+	}
+
+	if (req.method === "PATCH") {
+		console.log("Received PATCH request with data:", data);
+
+		try {
+			const client = await clientPromise;
+			const db = client.db("TeragramBallroom");
+			const employeeCollection = db.collection("employees");
+
+			const objectId = new ObjectId(id);
+			const filter = { _id: objectId };
+
+			let updateDocument;
+
+			if (Array.isArray(data.position)) {
+				updateDocument = {
+					$addToSet: { position: { $each: data.position || [] } },
+				};
+			} else {
+				updateDocument = {
+					$set: data,
+				};
+			}
+
+			const result = await employeeCollection.updateOne(filter, updateDocument);
+
+			console.log(
+				`${result.matchedCount} document(s) matched the filter criteria.`
+			);
+			console.log(`${result.modifiedCount} document(s) were modified.`);
+
+			res.status(200).json({ message: "Employee updated successfully" });
+		} catch (error) {
+			console.error("Error updating data:", error);
+			res.status(500).json({ error: "Internal Server Error" });
+		}
+	} else {
+		res.status(405).json({ message: "Method Not Allowed" });
+	}
+};
