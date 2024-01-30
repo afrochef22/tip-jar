5e07e4b574173b8cd491ff77fa4f6c5efb13ddd6
afrochef22 <70339140+afrochef22@users.noreply.github.com>
2024-01-30 15:04:37 -0800

    added _document.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,6 @@ import "../components/fontawesom";
 import Header from "../components/Header";
 
 import "../styles/globals.css";
-import Head from "next/head";
 
 function MyApp({ Component, pageProps, router }) {
 	const isNotIndexPage = router.pathname !== "/";
@@ -12,14 +11,6 @@ function MyApp({ Component, pageProps, router }) {
 	const additionalFeatures = isNotIndexPage ? <Header /> : null;
 	return (
 		<div className="background-color">
-			<Head>
-				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-				<link
-					href="https://fonts.googleapis.com/css2?family=Italiana&display=swap"
-					rel="stylesheet"
-				/>
-			</Head>
 			{additionalFeatures}
 			<Component {...pageProps} />
 		</div>
