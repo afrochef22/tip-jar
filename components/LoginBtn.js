38e270260e880ca9613d4b371656aa4de4e6c97e
afrochef22 <70339140+afrochef22@users.noreply.github.com>
2024-04-24 17:34:29 -0700

    added a call back [nextauth] and will update employee to add google id to sign in. Still not fully functioning though

--- components/LoginBtn.js
+++ components/LoginBtn.js
@@ -3,7 +3,7 @@ import { useSession, signIn, signOut } from "next-auth/react";
 export default function Component() {
 	const { data: session } = useSession();
 	if (session) {
-		console.log(session);
+		console.log("session", session);
 		return (
 			<>
 				<div className="login-button-container">
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,7 +8,7 @@ import EmailProvider from "next-auth/providers/email";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcrypt";
 
-export const authOptions = {
+const authOptions = {
 	// Configure one or more authentication providers
 	providers: [
 		CredentialsProvider({
@@ -54,6 +54,7 @@ export const authOptions = {
 		GoogleProvider({
 			clientId: process.env.GOOGLE_CLIENT_ID,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+			callbackUrl: `${process.env.NEXTAUTH_URL}/CCTipsTotals`, // Redirect URL after successful sign-in
 		}),
 
 		// EmailProvider({
@@ -80,27 +81,43 @@ export const authOptions = {
 
 	adapter: MongoDBAdapter(clientPromise),
 	callbacks: {
-		async signIn(user, account, profile) {
+		async signIn({ user, account, profile }) {
 			// Check if the account provider is Google
 			console.log("user: ", user);
+			console.log("account: ", account);
+			console.log("profile: ", profile);
+
 			if (account.provider === "google") {
+				console.log("is provider");
 				try {
 					const client = await clientPromise;
 					const db = client.db("TeragramBallroom");
 					const employeeCollection = db.collection("employees");
 
 					// Check if the user's Google ID is already in the database
+					console.log("profile ID", user.id);
 					const existingUser = await employeeCollection.findOne({
-						googleId: profile.id,
+						googleId: account.providerAccountId,
 					});
-
+					console.log("existingUser", existingUser);
 					if (!existingUser) {
 						console.log("not in data base");
 						// If the user's Google ID is not in the database, add it
-						await employeeCollection.updateOne(
-							{ email: profile.email },
-							{ $set: { googleId: profile.id } }
+						const result = await employeeCollection.updateOne(
+							{ firstName: profile.given_name },
+							{
+								$set: {
+									googleId: account.providerAccountId,
+									email: profile.email,
+								},
+							}
 						);
+						// Check if the update operation modified any documents
+						if (result.modifiedCount === 0) {
+							// If no documents were modified, return null
+							console.log("updateOne failed: no documents modified");
+							return null;
+						}
 					}
 				} catch (error) {
 					console.error("Error linking Google account:", error);
@@ -111,7 +128,5 @@ export const authOptions = {
 		},
 	},
 };
-
-const handler = NextAuth(authOptions);
 
 export default NextAuth(authOptions);
