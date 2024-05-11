1eddd44cea8683502ddae8ca25b500f573033e1f
afrochef22 <70339140+afrochef22@users.noreply.github.com>
2024-05-11 11:08:50 -0700

    made the sound pre load to help with the delay

--- components/ClickSound.js
+++ components/ClickSound.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 const ClickSound = ({ children }) => {
+	useEffect(() => {
+		const sound = new Audio("/sounds/click-low.wav");
+		sound.preload = "auto";
+	}, []);
 	const playSound = () => {
 		const sound = new Audio("/sounds/click-low.wav"); // Replace 'my-sound.mp3' with your sound file path
 		sound.play();
